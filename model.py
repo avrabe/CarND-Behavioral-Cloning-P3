4322214809c967f9475d8d93ad138796b72737c6
avrabe <ralf_beier@me.com>
2017-01-21 10:45:12 +0100

    Create a class which provides as source for data.

--- model.py
+++ model.py
@@ -1,12 +1,41 @@
+import os
+
+import attr
 import click
 
+
+def _search_driving_logs(self, attribute, value):
+    found = False
+    for root, dirs, files in os.walk(value, followlinks=False):
+        for name in files:
+            if name == "driving_log.csv":
+                found = True
+    if not found:
+        raise ValueError("No driving_log found")
+
+
+@attr.s
+class DrivingLogs:
+    base_path = attr.ib(default=".",
+                        validator=_search_driving_logs)
+
+    @property
+    def driving_logs(self):
+        return self._iterate_driving_logs()
+
+    def _iterate_driving_logs(self):
+        for root, dirs, files in os.walk(self.base_path, followlinks=False):
+            for name in files:
+                if name == "driving_log.csv":
+                    my_path = os.path.abspath(os.path.join(root, name))
+                    yield my_path
 
 @click.command()
 @click.option('--driving_logs', default=".",
               help='The roo path containing the driving logs and the corresponding images',
               type=click.Path(exists=True))
 def cli(driving_logs):
-    click.echo(driving_logs)
+    click.echo(DrivingLogs(driving_logs).driving_logs)
 
 
 if __name__ == '__main__':
--- test_model.py
+++ test_model.py
@@ -1,6 +1,20 @@
-def func(x):
-    return x + 1
+import os.path
+
+import pytest
+
+import model
 
 
-def test_answer():
-    assert func(3) == 4
+def test_all_driving_logs_csv():
+    dl = model.DrivingLogs()
+    assert len(list(dl.driving_logs)) == 2
+
+
+def test_one_driving_logs_csv():
+    dl = model.DrivingLogs(os.path.join("test_data", "one"))
+    assert len(list(dl.driving_logs)) == 1
+
+
+def test_no_driving_logs_csv():
+    with pytest.raises(ValueError):
+        model.DrivingLogs(os.path.join("test_data", "empty"))
