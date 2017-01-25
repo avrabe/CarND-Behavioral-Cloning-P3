c9ba817fc5ba4f06c7d19662feef03a96f494562
avrabe <ralf_beier@me.com>
2017-01-25 06:38:35 +0100

    Updated requirements and travis script.

--- model.py
+++ model.py
@@ -159,49 +159,51 @@ def cli(driving_logs):
     print(len(data))
     X_train, X_val, y_train, y_val = tts(data['center'], data['steering'])
 
-    model = Sequential()
-    model.add(
-        Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2), input_shape=(66, 200, 3), activation="relu"))
-    model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation="relu"))
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation="relu"))
-    model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(1164, activation="relu"))
-    model.add(Dense(100, activation="relu"))
-    model.add(Dense(50, activation="relu"))
-    model.add(Dense(10, activation="relu"))
-    model.add(Dense(1))
+    for i in ["sgd", "rmsprop", "adagrad", "adadelta", "adam", "adamax", "nadam"]:
+        model = Sequential()
+        model.add(
+            Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2), input_shape=(66, 200, 3), activation="relu"))
+        model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2), activation="relu"))
+        model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2), activation="relu"))
+        model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation="relu"))
+        model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation="relu"))
+        model.add(Flatten())
+        model.add(Dropout(0.5))
+        model.add(Dense(1164, activation="relu"))
+        model.add(Dense(100, activation="relu"))
+        model.add(Dense(50, activation="relu"))
+        model.add(Dense(10, activation="relu"))
+        model.add(Dense(1))
 
-    model.compile(loss='mse',
-                  optimizer='adam',
-                  )
+        model.compile(loss='mse',
+                      optimizer=i,
+                      )
 
-    samples_per_epoch = 15000
-    nb_val_samples = 2000
-    nb_epoch = 10
-    from keras.utils.visualize_util import model_to_dot
-    with open("foo.dot", 'w') as jfile:
-        jfile.write(model_to_dot(model, show_shapes=True, show_layer_names=True).to_string())
-    history = model.fit_generator(generate_arrays_from_file(X_train, y_train, 100, do_shuffle=True),
-                                  samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
-                                  validation_data=generate_arrays_from_file(X_val, y_val, 100),
-                                  nb_val_samples=nb_val_samples)
+        samples_per_epoch = 15000
+        nb_val_samples = 2000
+        nb_epoch = 10
+        from keras.utils.visualize_util import model_to_dot
+        with open(i + "_foo.dot", 'w') as jfile:
+            jfile.write(model_to_dot(model, show_shapes=True, show_layer_names=True).to_string())
+        history = model.fit_generator(generate_arrays_from_file(X_train, y_train, 100, do_shuffle=True),
+                                      samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
+                                      validation_data=generate_arrays_from_file(X_val, y_val, 100),
+                                      nb_val_samples=nb_val_samples)
 
-    # print("The cosine_proximity accuracy is: %.3f" % history.history['val_cosine_proximity'][-1])
-    # print("The loss is: %.3f" % history.history['val_loss'][-1])
-    with open("foo.json", 'w') as jfile:
-        json.dump(model.to_json(), jfile)
-    model.save_weights("foo.h5", overwrite=True)
+        # print("The cosine_proximity accuracy is: %.3f" % history.history['val_cosine_proximity'][-1])
+        # print("The loss is: %.3f" % history.history['val_loss'][-1])
+        with open(i + "_foo.json", 'w') as jfile:
+            json.dump(model.to_json(), jfile)
+        model.save_weights(i + _
+        "foo.h5", overwrite = True)
 
-    filename = "C:\\Users\\Ralf\\Downloads\\simulator-windows-64\\left_route\\IMG\\center_2017_01_19_19_25_49_380.jpg"
-    img = cv2.imread(filename)
-    img = cv2.resize(img, (200, 66))
+        filename = "C:\\Users\\Ralf\\Downloads\\simulator-windows-64\\left_route\\IMG\\center_2017_01_19_19_25_49_380.jpg"
+        img = cv2.imread(filename)
+        img = cv2.resize(img, (200, 66))
 
-    X = np.asarray([np.copy(img)])
-    steering_angle = float(model.predict(X, batch_size=1, verbose=1))
-    print(filename, "-0.307052", steering_angle)
+        X = np.asarray([np.copy(img)])
+        steering_angle = float(model.predict(X, batch_size=1, verbose=1))
+        print(i, filename, "-0.307052", steering_angle)
 
 
 if __name__ == '__main__':
