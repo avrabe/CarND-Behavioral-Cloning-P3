81d78918932cd11bef541ee215ee2feb5116b4ec
avrabe <ralf_beier@me.com>
2017-01-27 17:37:03 +0100

    Iterate through all kinds of optimizers and methods to check for the best.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import json
 import os
+from itertools import product
 
 import attr
 import click
@@ -9,6 +10,7 @@ import pandas as pd
 from keras.layers import Convolution2D, Dropout
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
+from keras.utils.visualize_util import model_to_dot
 from sklearn.model_selection import train_test_split as tts
 from sklearn.utils import shuffle
 
@@ -114,6 +116,12 @@ class DrivingLogs:
         """
         return pd.concat([_read_driving_log(x) for x in self.driving_logs])
 
+    @property
+    def train_test_split(self):
+        images = self.data_frame['center'].append(self.data_frame['left']).append(self.data_frame['right'])
+        steerings = self.data_frame['steering'].append(self.data_frame['steering']).append(self.data_frame['steering'])
+        return tts(images, steerings)
+
 
 def generate_arrays_from_file(x, y, batch_size, do_shuffle=False):
     batch_count = 0
@@ -129,8 +137,10 @@ def generate_arrays_from_file(x, y, batch_size, do_shuffle=False):
             # create numpy arrays of input data
             # and labels, from each line in the file
             img = cv2.imread(filename)
+            if img is None:
+                print("Skip unreadable %s" % filename)
+                break
             img = cv2.resize(img, (200, 66))
-
             X.append(np.copy(img))
             Y.append(steering)
             # print(steering, filename, img.shape)
@@ -150,16 +160,24 @@ def generate_arrays_from_file(x, y, batch_size, do_shuffle=False):
                 Y = []
 
 
+def get_filename(optimize, objective, epoch, suffix=".json"):
+    return "%s_%s_e%02d%s" % (optimize, objective, epoch, suffix)
+
+
 @click.command()
 @click.option('--driving_logs', default=".",
               help='The roo path containing the driving logs and the corresponding images',
               type=click.Path(exists=True))
 def cli(driving_logs):
-    data = DrivingLogs(driving_logs).data_frame
-    print(len(data))
-    X_train, X_val, y_train, y_val = tts(data['center'], data['steering'])
+    X_train, X_val, y_train, y_val = DrivingLogs(driving_logs).train_test_split
+    print(len(X_train), len(X_val))
 
-    for i in ["sgd", "rmsprop", "adagrad", "adadelta", "adam", "adamax", "nadam"]:
+    optimizers = ["sgd", "rmsprop", "adagrad", "adadelta", "adam", "adamax", "nadam"]
+    objectives = ["mse", "mae", "mape", "msle", "squared_hinge", "hinge"]
+    epochs = range(1, 20, 4)
+
+    for optimizer, objective, nb_epoch in product(optimizers, objectives, epochs):
+        print("===== %s ===== %s ===== %02d =====" % (optimizer, objective, nb_epoch))
         model = Sequential()
         model.add(
             Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2), input_shape=(66, 200, 3), activation="relu"))
@@ -175,26 +193,25 @@ def cli(driving_logs):
         model.add(Dense(10, activation="relu"))
         model.add(Dense(1))
 
-        model.compile(loss='mse',
-                      optimizer=i,
-                      )
+        model.compile(loss=objective,
+                      optimizer=optimizer,
+                      metrics=["mean_squared_error"])
 
         samples_per_epoch = 15000
         nb_val_samples = 2000
-        nb_epoch = 10
-        from keras.utils.visualize_util import model_to_dot
-        with open(i + "_foo.dot", 'w') as jfile:
+
+        with open(get_filename(optimizer, objective, nb_epoch, suffix=".dot"), 'w') as jfile:
             jfile.write(model_to_dot(model, show_shapes=True, show_layer_names=True).to_string())
         history = model.fit_generator(generate_arrays_from_file(X_train, y_train, 100, do_shuffle=True),
                                       samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
                                       validation_data=generate_arrays_from_file(X_val, y_val, 100),
                                       nb_val_samples=nb_val_samples)
 
-        # print("The cosine_proximity accuracy is: %.3f" % history.history['val_cosine_proximity'][-1])
-        # print("The loss is: %.3f" % history.history['val_loss'][-1])
-        with open(i + "_foo.json", 'w') as jfile:
+        print("The cosine_proximity accuracy is: %.3f" % history.history['val_mean_squared_error'][-1])
+        print("The loss is: %.3f" % history.history['val_loss'][-1])
+        with open(get_filename(optimizer, objective, nb_epoch), 'w') as jfile:
             json.dump(model.to_json(), jfile)
-        model.save_weights(i + "_foo.h5", overwrite=True)
+        model.save_weights(get_filename(optimizer, objective, nb_epoch, suffix=".h5"), overwrite=True)
 
         filename = "C:\\Users\\Ralf\\Downloads\\simulator-windows-64\\left_route\\IMG\\center_2017_01_19_19_25_49_380.jpg"
         img = cv2.imread(filename)
@@ -202,7 +219,7 @@ def cli(driving_logs):
 
         X = np.asarray([np.copy(img)])
         steering_angle = float(model.predict(X, batch_size=1, verbose=1))
-        print(i, filename, "-0.307052", steering_angle)
+        print(optimizer, objective, nb_epoch, filename, "-0.307052", steering_angle)
 
 
 if __name__ == '__main__':
